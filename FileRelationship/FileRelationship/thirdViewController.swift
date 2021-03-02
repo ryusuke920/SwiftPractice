f8ab6999aed00f0dbfd1605afb36e828f11dbdbd
ryusuke <ryusuke_920@icloud.com>
2021-03-02 21:40:55 +0900

    add ScreenTransition and FileRelationship

---
+++ FileRelationship/FileRelationship/thirdViewController.swift
@@ -0,0 +1,31 @@
+//
+//  thirdViewController.swift
+//  FileRelationship
+//
+//  Created by 日置竜輔 on 2021/03/02.
+//
+
+import UIKit
+
+class thirdViewController: UIViewController {
+    @IBOutlet weak var label3: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        label3.text = "３つ目の画面"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
